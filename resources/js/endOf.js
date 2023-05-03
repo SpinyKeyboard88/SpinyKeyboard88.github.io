e303d83011425614e15c87a7c2b29db58823fd00
SpinyKeyboard88 <juan.porraz84@gmail.com>
2023-05-03 14:18:23 -0500

    almost finished with microTalk page
    Added some javascript for the tabbing feature that i wanted. Soon i'm going to be embedding an iframe onto the site for the YouTube video that i was going to make.

---
+++ resources/js/endOf.js
@@ -0,0 +1,21 @@
+function openPage(pageName, tabItem) {
+    //settingUpEquipment
+    //configuringMixPre10
+    //exportingProject
+    var i, j;
+    var visible;
+    const tabcontent = document.getElementsByClassName("section");
+    for (let index = 0; index < tabcontent.length; index++) {
+        i = tabcontent.item(index);
+        j = document.getElementById(i.id);
+        j.classList.add("hidden");
+        j.classList.remove("unhidden");
+    }
+
+    visible = document.getElementById(pageName);
+    visible.classList.remove("hidden");
+    visible.classList.add("unhidden");
+
+    tabItem.classList.add("")
+
+}
